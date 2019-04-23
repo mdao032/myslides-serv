37750ad706f39993b85f404bac0c0a94c4b89df6
mdao032 <mdao032@gmail.com>
2019-04-24 01:34:20 +0200

    Test with mongoClient

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 
@@ -8,4 +8,33 @@ var server = app.listen(8080, function () {
     var port = server.address().port;
 
     console.log("express server is now running");
-})
+});
+
+
+/*
+MongoClient.connect('mongodb://localhost:27017/firmelog_db', function(err, db) {
+    if (err) {
+        throw err;
+    }
+    db.collection('user').find().toArray(function(err, result) {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+    });
+});
+*/
+
+app.get('/user', function(req, res){
+    MongoClient.connect('mongodb://localhost:27017/firmelog_db', function(err, db) {
+        if (err) {
+            throw err;
+        }
+        db.collection('user').find().toArray(function(err, result) {
+            if (err) {
+                throw err;
+            }
+            res.send(result);
+        });
+    });
+});
